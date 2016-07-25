a7237a3ec8ec00bc6ac8c12b94a9516dcec9ac14
Wayne <Wayne@appledeMacBook-Air.local>
2016-07-25 19:52:28 +0900

    feedforward

--- nn.py
+++ nn.py
@@ -21,14 +21,14 @@ class searchnet:
         else:
             table = 'hiddenurl'
         res = self.con.execute(
-            'select from %s where fromid = %d and toid = %d' %
-            (table,fromid, toid)).fetchone()
+            'select strength from %s where fromid = %d and toid = %d' %
+            (table, fromid, toid)).fetchone()
         if res==None:
             if layer==0:
                 return -0.2
             if layer==1:
                 return 0
-            return res[0]
+        return res[0]
 
     def setstrength(self, fromid, toid, layer, strength):
         if layer==0:
@@ -52,10 +52,10 @@ class searchnet:
             return None
         createkey = '_'.join(sorted([str(wi) for wi in wordids]))
         res = self.con.execute(
-            'select rowid from hiddennode where create_key= %s' % createkey).fetchone()
-        if res = None:
+            "select rowid from hiddennode where create_key= '%s'" % createkey).fetchone()
+        if res == None:
             cur = self.con.execute(
-                'insert into hiddennode (create_key) values %s' % createkey)
+                "insert into hiddennode (create_key) values ('%s')" % createkey)
             hiddenid = cur.lastrowid
 
             for wordid in wordids:
@@ -64,7 +64,72 @@ class searchnet:
                 self.setstrength(hiddenid,urlid,1,0.1)
             self.con.commit()
 
+    def getallhiddenids(self, wordids, urlids):
+        l1={}
+        for wordid in wordids:
+            cur = self.con.execute(
+                'select toid from wordhidden where fromid = %d' % wordid)
+            for row in cur:
+                l1[row[0]]=1
+        for urlid in urlids:
+            cur = self.con.execute(
+                'select fromid from hiddenurl where toid=%d' % urlid)
+            for row in cur:
+                l1[row[0]]=1
+        return l1.keys()
 
+    def setupnetwork(self, wordids, urlids):
+        self.wordids = wordids
+        self.hiddenids = self.getallhiddenids(wordids, urlids)
+        self.urlids = urlids
+
+        self.ai = [1.0] * len(self.wordids)
+        self.ah = [1.0] * len(self.hiddenids)
+        self.ao = [1.0] * len(self.urlids)
+
+        self.wi = [[self.getstrength(wordid, hiddenid, 0)
+                   for hiddenid in self.hiddenids]
+                   for wordid in self.wordids]
+
+        self.wo = [[self.getstrength(hiddenid, urlid, 1)
+                    for urlid in self.urlids]
+                    for hiddenid in self.hiddenids]
+
+    def feedforward(self):
+        for i in range(len(self.wordids)):
+            self.ai[i] = 1.0
+
+        for j in range(len(self.hiddenids)):
+            sum = 0.0
+            for i in range(len(self.wordids)):
+                sum = sum + self.ai[i] * self.wi[i][j]
+            self.ah[j] = tanh(sum)
+
+            for k in range(len(self.urlids)):
+                sum = 0.0
+                for j in range(len(self.hiddenids)):
+                    sum = sum + self.ah[j] * self.wo[j][k]
+                self.ao[k] = tanh(sum)
+            return self.ao[:]
+
+
+    def getresult(self, wordids, urlids):
+        self.setupnetwork(wordids, urlids)
+        print self.feedforward()
+        return self.feedforward()
+
+
+
+
+mynet = searchnet('nn.db')
+# 
+wWorld, wRiver, wBank = 101,102,103
+uworldBank, uRiver,uEarth = 201,202,203
+mynet.getresult([wWorld,wBank],[uworldBank, uRiver,uEarth])
+# for c in mynet.con.execute('select * from wordhidden'):
+#     print c
+# for c in mynet.con.execute('select * from hiddenurl'):
+#     print c
 
 
 
